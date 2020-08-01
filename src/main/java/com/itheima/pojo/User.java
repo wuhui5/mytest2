2fdfc1fd9a96946bc286d1026e3a80b8ad384ff2
wuhui <3141762113wh@gmail.com>
2020-08-01 17:20:52 +0800

    第三次递交

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,53 @@
+package com.itheima.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+    private String email;
+    private String gender;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
